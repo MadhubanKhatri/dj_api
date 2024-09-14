761d631199aec0e419492952a4ac2696add9e6a8
Madhuban Khatri <madhuban211998@gmail.com>
2024-09-14 22:33:03 +0530

    user registration api

--- api/api/urls.py
+++ api/api/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import RegisterView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/register/', RegisterView.as_view(), name='auth_register'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
---
+++ api/app/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username','email','password')
+
+        def create(self, validated_data):
+            user = User.objects.create(validated_data['username'], validated_data['email'], validated_data['password'])
+            return user
